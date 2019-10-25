132f485773c4f520c389546375fc780ffdd88421
Dante12129 <dan9coasterdan@gmail.com>
2019-10-25 01:23:22 -0400

    Dark Mode

--- phona-notif-frontend/src/App.vue
+++ phona-notif-frontend/src/App.vue
@@ -18,11 +18,15 @@ export default {
 </script>
 
 <style>
+html {
+  background: black;
+  color: white;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
   margin: 0 auto;
   width: 50%;
 }
