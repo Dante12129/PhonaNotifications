2826c2c72280eebb1c54f3e51ae406f8618ca64d
Dante12129 <dan9coasterdan@gmail.com>
2019-10-25 01:15:33 -0400

    Basic Login Page

--- phona-notif-frontend/src/App.vue
+++ phona-notif-frontend/src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--    <img alt="Vue logo" src="./assets/logo.png">-->
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <Login></Login>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Login from "@/components/Login";
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Login
   }
 }
 </script>
@@ -21,8 +22,8 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0 auto;
+  width: 50%;
 }
 </style>
---
+++ phona-notif-frontend/src/components/Login.vue
@@ -0,0 +1,40 @@
+<template>
+  <form v-on:click.prevent="">
+    <div class="form-input flow">
+      <label for="username">Username: </label>
+      <input type="text" id="username">
+    </div>
+    <div class="form-input flow">
+      <label for="password">Password: </label>
+      <input type="text" id="password">
+    </div>
+    <button type="submit" class="form-input">Submit</button>
+  </form>
+</template>
+
+<script>
+  export default {
+    name: "Login"
+  }
+</script>
+
+<style scoped>
+  .form-input {
+    margin: 5px auto;
+  }
+  div.form-input {
+    width: 250px;
+  }
+  button.form-input {
+    display: block;
+    width: 150px;
+  }
+
+  .flow {
+    display: flex;
+    flex-direction: row;
+  }
+  .flow > * {
+    flex: 1;
+  }
+</style>
