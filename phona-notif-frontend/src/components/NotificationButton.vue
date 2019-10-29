a57b5d269597dbd3da50956fc39198021e81c693
Dante12129 <dan9coasterdan@gmail.com>
2019-10-29 12:42:50 -0400

    Create Buttons For Notifications

---
+++ phona-notif-frontend/src/components/NotificationButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <button>
+    {{ prettyKind }}
+  </button>
+</template>
+
+<script>
+  export default {
+    name: "NotificationButton",
+    props: {
+      kind: {
+        type: String,
+        required: true
+      }
+    },
+    computed: {
+      prettyKind: function () {
+        return `${this.kind.charAt(0).toUpperCase()}${this.kind.substr(1, this.kind.length - 1)}`
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- phona-notif-frontend/src/components/User.vue
+++ phona-notif-frontend/src/components/User.vue
@@ -1,21 +1,38 @@
 <template>
-  <div>
+  <div class="user">
     <h1>{{ name }}</h1>
+    <div class="buttons">
+      <NotificationButton kind="gift"></NotificationButton>
+      <NotificationButton kind="issue"></NotificationButton>
+      <NotificationButton kind="question"></NotificationButton>
+    </div>
   </div>
 </template>
 
 <script>
+  import NotificationButton from "@/components/NotificationButton";
   export default {
     name: "User",
+    components: {NotificationButton},
     props: {
       name: {
         type: String,
-        default: "NoUser"
+        required: true
       }
     }
   }
 </script>
 
 <style scoped>
+  .user {
+    text-align: center;
+  }
 
+  .buttons {
+    display: flex;
+  }
+
+  .buttons > button {
+    flex-grow: 1;
+  }
 </style>
