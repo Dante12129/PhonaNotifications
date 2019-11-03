1083b961382fc352e23680c49ff986ce613e4cd0
Dante12129 <dan9coasterdan@gmail.com>
2019-11-03 01:35:05 -0500

    Button Colors

--- phona-notif-frontend/src/components/NotificationButton.vue
+++ phona-notif-frontend/src/components/NotificationButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button>
+  <button @click="sendNotification" :class="kind">
     {{ prettyKind }}
   </button>
 </template>
@@ -15,12 +15,47 @@
     },
     computed: {
       prettyKind: function () {
-        return `${this.kind.charAt(0).toUpperCase()}${this.kind.substr(1, this.kind.length - 1)}`
+        return `${this.kind.charAt(0).toUpperCase()}${this.kind.substr(1, this.kind.length - 1)}`;
+      }
+    },
+    methods: {
+      sendNotification: function () {
+        console.log(this.prettyKind);
       }
     }
   }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+  $gift-color: #00ff00;
+  $issue-color: #ffff00;
+  $question-color: #00ffff;
 
+  button {
+    border: 1px solid black;
+    padding: 0;
+    height: 2em;
+    font-weight: bold;
+  }
+
+  .gift {
+    background-color: $gift-color;
+  }
+  .gift:hover {
+    background-color: darken($gift-color, 5%);
+  }
+
+  .issue {
+    background-color: $issue-color;
+  }
+  .issue:hover {
+    background-color: darken($issue-color, 5%);
+  }
+
+  .question {
+    background-color: $question-color;
+  }
+  .question:hover {
+    background-color: darken($question-color, 5%);
+  }
 </style>
